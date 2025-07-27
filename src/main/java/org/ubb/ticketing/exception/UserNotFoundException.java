46af25b500a6afafc7087ef3049531bd231dbc2e
minerkg <129666850+minerkg@users.noreply.github.com>
2025-07-27 22:17:19 +0300

    complaint service methods added

---
+++ src/main/java/org/ubb/ticketing/exception/UserNotFoundException.java
@@ -0,0 +1,16 @@
+package org.ubb.ticketing.exception;
+
+public class UserNotFoundException extends TicketingSystemException {
+
+    public UserNotFoundException(String message) {
+        super(message);
+    }
+
+    public UserNotFoundException(String message, Throwable cause) {
+        super(message, cause);
+    }
+
+    public UserNotFoundException(Throwable cause) {
+        super(cause);
+    }
+}
--- src/main/java/org/ubb/ticketing/service/ComplaintTicketService.java
+++ src/main/java/org/ubb/ticketing/service/ComplaintTicketService.java
@@ -1,13 +1,18 @@
 package org.ubb.ticketing.service;
 
 
+import jakarta.transaction.Transactional;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
+import org.ubb.ticketing.domain.SolutionType;
+import org.ubb.ticketing.domain.TicketStatus;
 import org.ubb.ticketing.domain.complaint.ComplaintTicket;
+import org.ubb.ticketing.domain.user.TicketingUser;
 import org.ubb.ticketing.exception.TicketNotFoundException;
 import org.ubb.ticketing.repository.ComplaintTicketRepository;
 
+import java.time.LocalDateTime;
 import java.util.HashSet;
 import java.util.Set;
 
@@ -49,6 +54,73 @@ public class ComplaintTicketService {
                         () -> new TicketNotFoundException("No complaint ticket with id " + id)
                 );
         complaintTicketRepository.deleteById(id);
+    }
+
+    @Transactional
+    public ComplaintTicket assignTicket(Long id, TicketingUser assignedTo, TicketingUser currentUser) {
+        logger.debug("assignTicket complaint ticket accessed in service");
+        var ticket = complaintTicketRepository
+                .findById(id).orElseThrow(
+                        () -> new TicketNotFoundException("No complaint ticket with id " + id)
+                );
+
+        //TODO: check if the user exists and cross save the modifications
+//        var assignedToUser = userRepository
+//                .findByName(assignedTo).orElseThrow(
+//                        () -> new UserNotFoundException("No user with name " + assignedTo)
+//                );
+        //TODO: check if the current user is the user who wants to assign to himself the ticket wit any role or has supervisor role
+
+        ticket.setAssignedTo(assignedTo);
+        ticket.setTicketStatus(TicketStatus.ASSIGNED);
+        return ticket;
+    }
+
+
+    @Transactional
+    public ComplaintTicket workingOnTicket(Long id, TicketingUser currentUser) {
+        logger.debug("assignTicket complaint ticket accessed in service");
+        var ticket = complaintTicketRepository
+                .findById(id).orElseThrow(
+                        () -> new TicketNotFoundException("No complaint ticket with id " + id)
+                );
+        //TODO: check if the user exists and cross save the modifications
+//        var assignedToUser = userRepository
+//                .findByName(assignedTo).orElseThrow(
+//                        () -> new UserNotFoundException("No user with name " + assignedTo)
+//                );
+        //TODO: check if the current user is the assigned user otherwise notify the user
+
+        ticket.setTicketStatus(TicketStatus.IN_PROGRESS);
+        return ticket;
+    }
+
+
+    @Transactional
+    public ComplaintTicket closeTicket(Long id, TicketingUser currentUser,
+                                       String solutionDescription,
+                                       SolutionType solutionType) {
+        logger.debug("closeTicket complaint ticket accessed in service");
+        var ticket = complaintTicketRepository
+                .findById(id).orElseThrow(
+                        () -> new TicketNotFoundException("No complaint ticket with id " + id)
+                );
+
+
+        //TODO: check if the user is granted to close tickets and the ticket is assignet to teh current user
+
+        ticket.setSolutionDescription(solutionDescription);
+        ticket.setSolutionType(solutionType);
+
+        ticket.setClosedBy(currentUser);
+        ticket.setClosedWhen(LocalDateTime.now());
+
+
+        //TODO: send notification about the closeing event
+
+        ticket.setTicketStatus(TicketStatus.CLOSED);
+
+        return ticket;
     }
 
 
