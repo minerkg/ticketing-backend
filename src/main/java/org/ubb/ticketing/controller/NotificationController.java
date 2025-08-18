b77070df8c5bafb8b4c7b97ebbbd2a7db2582b15
minerkg <129666850+minerkg@users.noreply.github.com>
2025-08-18 16:12:30 +0300

    websocket comm added

---
+++ src/main/java/org/ubb/ticketing/controller/NotificationController.java
@@ -0,0 +1,17 @@
+package org.ubb.ticketing.controller;
+
+import org.springframework.messaging.handler.annotation.MessageMapping;
+import org.springframework.messaging.handler.annotation.SendTo;
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class NotificationController {
+
+    @MessageMapping("/sendMessage") // Endpoint matching the JavaScript destination
+    @SendTo("/topic/notifications") // Broadcast to subscribers of this topic
+    public String sendMessage(String message) {
+        System.out.println("Received message: " + message); // Debugging log
+        return message; // Broadcast the message
+    }
+
+}
