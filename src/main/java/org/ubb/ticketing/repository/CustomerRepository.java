44ddd8a560ea010d7131b0880c1ba89a7488273b
minerkg <129666850+minerkg@users.noreply.github.com>
2025-09-06 11:06:13 +0300

    customer validation added

--- src/main/java/org/ubb/ticketing/repository/CustomerRepository.java
+++ src/main/java/org/ubb/ticketing/repository/CustomerRepository.java
@@ -4,9 +4,13 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import org.ubb.ticketing.domain.customer.Customer;
 
+import java.util.Optional;
 import java.util.UUID;
 
 @Repository
 public interface CustomerRepository extends JpaRepository<Customer, UUID> {
 
+    Optional<Customer> findByPhoneNumber(String phoneNumber);
+
+    Optional<Customer> findByEmail(String email);
 }
